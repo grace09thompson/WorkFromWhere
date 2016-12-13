6c0435b3001561811d033a89d77e4ccb01204a9c
grace09thompson <gracet2@myuw.net>
2016-12-13 14:51:14 -0800

    bare bones calendar set up

---
+++ WorkFromWhere/CellView.swift
@@ -0,0 +1,15 @@
+//
+//  CellView.swift
+//  WorkFromWhere
+//
+//  Created by Grace Thompson on 12/13/16.
+//  Copyright © 2016 Grace Thompson. All rights reserved.
+//
+
+import Foundation
+import JTAppleCalendar
+
+
+class CellView: JTAppleDayCellView {
+    @IBOutlet var dayLabel: UILabel!
+}
--- WorkFromWhere/ViewController.swift
+++ WorkFromWhere/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CellView") //Register the cell
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    //function to set up data-source protocol for calendar
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let startDate = formatter.date(from: "2016 12 01")!  //you can use date generated from a formatter
+        let endDate = Date()
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                endDate: endDate,
+                                                numberOfRows: 6,
+                                                calendar: Calendar.current,
+                                                generateInDates: .forAllMonths,
+                                                generateOutDates: .tillEndOfGrid,
+                                                firstDayOfWeek: .sunday)
+        return parameters
+    }
+    //delegate protocol method to see date cells
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+        let myCustomCell = cell as! CellView
+        
+        //setup cell text
+        myCustomCell.dayLabel.text = cellState.text
+        
+        //setup text color 
+        if cellState.dateBelongsTo == .thisMonth {
+            myCustomCell.dayLabel.textColor = UIColor.black
+        } else {
+            myCustomCell.dayLabel.textColor = UIColor.gray
+        }
+    }
+}
